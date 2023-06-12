e9580e03fcfef2e8ec31031813da14da5eddce9d
skyespirates <akashiseijurou414@gmail.com>
2023-06-12 22:04:40 +0700

    improve responsivity and theme mode

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,48 @@
 import "./style.css";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h3>About Page</h3>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, numquam
+          alias iste doloribus non quidem ea consequuntur officia veritatis
+          debitis unde sequi architecto sit, tenetur nam saepe, maxime
+          accusantium. Eligendi sapiente neque harum cum quibusdam aperiam eaque
+          exercitationem obcaecati cumque ut nihil natus, nemo deleniti officia
+          reprehenderit atque iure accusamus quam accusantium aspernatur facere
+          consequatur rerum? Voluptatibus vitae est repellendus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, numquam
+          alias iste doloribus non quidem ea consequuntur officia veritatis
+          debitis unde sequi architecto sit, tenetur nam saepe, maxime
+          accusantium. Eligendi sapiente neque harum cum quibusdam aperiam eaque
+          exercitationem obcaecati cumque ut nihil natus, nemo deleniti officia
+          reprehenderit atque iure accusamus quam accusantium aspernatur facere
+          consequatur rerum? Voluptatibus vitae est repellendus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, numquam
+          alias iste doloribus non quidem ea consequuntur officia veritatis
+          debitis unde sequi architecto sit, tenetur nam saepe, maxime
+          accusantium. Eligendi sapiente neque harum cum quibusdam aperiam eaque
+          exercitationem obcaecati cumque ut nihil natus, nemo deleniti officia
+          reprehenderit atque iure accusamus quam accusantium aspernatur facere
+          consequatur rerum? Voluptatibus vitae est repellendus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, numquam
+          alias iste doloribus non quidem ea consequuntur officia veritatis
+          debitis unde sequi architecto sit, tenetur nam saepe, maxime
+          accusantium. Eligendi sapiente neque harum cum quibusdam aperiam eaque
+          exercitationem obcaecati cumque ut nihil natus, nemo deleniti officia
+          reprehenderit atque iure accusamus quam accusantium aspernatur facere
+          consequatur rerum? Voluptatibus vitae est repellendus.
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./style.css";
 
 const Header = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
-  const handleClick = () => {
-    console.log(show);
-    setShow(!show);
+  const body = document.querySelector("body");
+  const btn = document.getElementById("theme-button");
+
+  // Dark/Light Theme
+  const lightTheme = "light-theme";
+  const iconTheme = "bxs-moon";
+
+  // Previosly selected theme
+  const selectedTheme = localStorage.getItem("selected-theme");
+  const selectedIcon = localStorage.getItem("selected-icon");
+
+  // Obtain current theme and icon
+  const getCurrentTheme = () =>
+    body?.classList.contains(lightTheme) ? "light" : "dark";
+  const getCurrentIcon = () =>
+    btn?.classList.contains(iconTheme) ? "bxs-sun" : "bxs-moon";
+
+  useEffect(() => {
+    if (selectedTheme) {
+      document.body.classList[selectedTheme === "light" ? "add" : "remove"](
+        lightTheme
+      );
+      btn?.classList[selectedIcon === "bxs-sun" ? "add" : "remove"](iconTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const btn = document.getElementById("theme-button");
+    console.log(btn);
+    body?.classList.toggle(lightTheme);
+    btn?.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
   };
   return (
     <header>
@@ -16,21 +47,30 @@ const Header = () => {
         </a>
         <div className={`nav__menu ${show ? "show-menu" : ""}`}>
           <ul className="nav__items">
-            <li className="nav__item">
-              <a className="nav__link" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">
-                About
-              </a>
-            </li>
+            <NavLink
+              onClick={() => setShow(!show)}
+              to="/"
+              className="nav__item"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              onClick={() => setShow(!show)}
+              to="/about"
+              className="nav__item"
+            >
+              About
+            </NavLink>
           </ul>
         </div>
         {/* Nav Buttons */}
         <div className="nav__buttons">
-          <i className="bx bxs-moon"></i>
+          <button className="toggle__theme" onClick={toggleTheme}>
+            <i className="bx bxs-sun bxs-moon" id="theme-button"></i>
+          </button>
+          <a href="https://github.com/skyespirates/taskSky" target="_blank">
+            <i className="bx bxl-github"></i>
+          </a>
           <div className="toggle__menu" onClick={() => setShow(!show)}>
             {show ? (
               <i className="bx bx-x"></i>
